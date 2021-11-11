b5e3f00ae10971cf7865fbb3d34fcf4f7b4839e1
Mike Eshva <reg@eshva.ru>
2021-11-11 20:30:54 +0300

    Убрать sidebar, который не поддерживается

--- documents/about/conf.py
+++ documents/about/conf.py
@@ -105,8 +105,7 @@ html_sidebars = {
     '**': [
         'relations.html',  # needs 'show_related': True theme option to display
         'searchbox.html',
-        'localtoc.html',
-        'navigation.html'
+        'localtoc.html'
     ]
 }
 
